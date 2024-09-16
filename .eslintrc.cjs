c729f71029cef40ae4b26e67ef9adb000b3b8f92
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-09-16 15:28:39 +0200

    🐛

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script',
-        project: "./tsconfig.json"
+        project: 'tsconfig.json'
       },
     },
   ],
